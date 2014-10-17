501259f69a7c9ae86ebf6c3d4ee31b2ffc18dcdb
Rolandas Barysas <r.b@riseup.net>
2014-10-17 12:55:02 +0300

    added few comments [ci skip]

--- lib/gogcom.rb
+++ lib/gogcom.rb
@@ -1,9 +1,17 @@
 module Gogcom
   class << self
+    # Gets game information from website.
+    #
+    # @param [string] Game name
     def game(name)
       Gogcom::Game.get(name)
     end
 
+    # Gets all items on sale.
+    #
+    # @param [Hash]
+    #   :type => "games"
+    #   :type => "movies" 
     def sale(options = {})
     	Gogcom::Sale.get(options)
     end
