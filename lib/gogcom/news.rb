e4313bbbe19bcab598610acf4ed0f488bd12e97d
Rolandas Barysas <r.b@riseup.net>
2014-10-21 10:20:50 +0300

    news will return nil on error

--- lib/gogcom/news.rb
+++ lib/gogcom/news.rb
@@ -9,23 +9,27 @@ module Gogcom
 		end
 
 		def self.parse_data(data, options = {})
-			rss = SimpleRSS.parse data
-			news = Array.new
-			limit = options[:limit] || nil
-			count = 0
+			begin
+				rss = SimpleRSS.parse data
+				news = Array.new
+				limit = options[:limit] || nil
+				count = 0
 
-			rss.items.each do |item|
-				news_item = News.new
-				
-				news_item.title = item.title
-				news_item.link = item.link
-				news_item.body = item.description.force_encoding("UTF-8")
-				news_item.date = item.pubDate
+				rss.items.each do |item|
+					news_item = News.new
+					
+					news_item.title = item.title
+					news_item.link = item.link
+					news_item.body = item.description.force_encoding("UTF-8")
+					news_item.date = item.pubDate
 
-				news.push news_item
-				count += 1
-				
-				break if !limit.nil? && count >= limit
+					news.push news_item
+					count += 1
+					
+					break if !limit.nil? && count >= limit
+				end
+			rescue
+				news = nil
 			end
 
 			news
