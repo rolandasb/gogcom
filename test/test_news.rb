da268446f65e5c86b2b1df99b7086c0087900364
Rolandas Barysas <r.b@riseup.net>
2014-10-28 23:56:21 +0200

    formatting [ci skip]

--- test/test_news.rb
+++ test/test_news.rb
@@ -1,22 +1,22 @@
 require 'helper'
 
 class GogcomNewsTest < Minitest::Test
-	def setup
-		VCR.use_cassette('news') do
-			@news = Gogcom.news()
-		end
+  def setup
+    VCR.use_cassette('news') do
+      @news = Gogcom.news()
+    end
 
-		VCR.use_cassette('news') do
-			@news_limit = Gogcom.news(:limit => 2)
-		end
-	end
+    VCR.use_cassette('news') do
+      @news_limit = Gogcom.news(:limit => 2)
+    end
+  end
 
-	def test_news
-		assert @news.is_a?(Array)
-	end
+  def test_news
+    assert @news.is_a?(Array)
+  end
 
-	def test_news_limit
-		assert @news_limit.is_a?(Array)
-		assert @news_limit.count, 2
-	end
+  def test_news_limit
+    assert @news_limit.is_a?(Array)
+    assert @news_limit.count, 2
+  end
 end
